c0b75d70f3107f7bc8c0f104b561cb8824688260
DrissHaddadi <driss.haddadi@outlook.fr>
2023-06-14 17:29:32 +0200

    I created the next.app, installed tailwind and created the first thee pages as well as my first next component

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <header>
+      <div className=" ">
+        <div className="">Logo</div>
+        <nav>
+          <ul className=" ">
+            <li className="text-red-600">Accueil</li>
+            <li className="text-green-600">Menu</li>
+            <li className="text-yellow-600">Contact</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ pages/Menu.js
@@ -0,0 +1,10 @@
+import Header from '@/components/Header';
+
+export default function Menu() {
+  return (
+    <>
+      <Header />
+      <h1>Hello Menu</h1>
+    </>
+  );
+}
