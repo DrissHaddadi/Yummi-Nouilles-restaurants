301ee22a88de4d0b8a07329239e1781d1fbc01e1
DrissHaddadi <driss.haddadi@outlook.fr>
2023-06-19 16:09:36 +0200

    I created a dedicated font.css file to import fonts from google font and create custom classes for the different typography style of the app. I then imported it and used it on the CTA_Button and CTA_Banner components

--- components/CTA_Banner.js
+++ components/CTA_Banner.js
@@ -1,9 +1,12 @@
+import '../styles/font.css';
 import CTA_Button from './CTA_Button';
 
 const CTA_Banner = () => {
   return (
     <div className="w-full h-52 bg-yummy-red flex flex-col items-center justify-center">
-      <div className="text-slate-50 py-2">Commandez dès maintenant !</div>
+      <div className="text-slate-50 BodyHighlight py-2">
+        Commandez maintenant !
+      </div>
       <div className="py-2">
         <CTA_Button />
       </div>
--- components/CTA_Button.js
+++ components/CTA_Button.js
@@ -1,7 +1,9 @@
+import '../styles/font.css';
+
 const CTA_Button = () => {
   return (
-    <div className="w-56 h-14 bg-black ">
-      <div className="text-slate-50">click & collect</div>
+    <div className="w-56 h-14 bg-black flex items-center justify-center ">
+      <div className="text-slate-50 Title1 text-center">click & collect</div>
     </div>
   );
 };
