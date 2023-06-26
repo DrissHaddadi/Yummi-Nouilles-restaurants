bebfeb11f63ad2b9a9dce8abb584c17ff860acbf
DrissHaddadi <driss.haddadi@outlook.fr>
2023-06-26 10:51:17 +0200

    I made the navigation bar fonctionnal and added a conditionnal underline effect so the user can know which page he/she is currently on

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,17 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import '../styles/font.css';
 
 const Header = () => {
+  const router = useRouter();
+  const [activeLink, setActiveLink] = useState('');
+
+  useEffect(() => {
+    setActiveLink(router.pathname);
+  }, [router.pathname]);
+
   return (
     <header>
       <div className="container mx-auto pt-4 pl-7 h-32 mb-2">
@@ -14,11 +24,38 @@ const Header = () => {
               alt="Picture of the logo"
             />
           </div>
-          <nav className="flex  items-center">
+          <nav className="flex items-center">
             <ul className="m-2 flex flex-row content-center">
-              <li className="m-2 Title1 ">Accueil</li>
-              <li className="m-2 Title1 ">Menu</li>
-              <li className="m-2 Title1 ">Contact</li>
+              <li>
+                <Link href="/" className="m-2 Title1 inline-flex">
+                  <span className="relative">
+                    Accueil
+                    {activeLink === '/' && (
+                      <span className="absolute inset-x-0 bottom-0 h-1 bg-yummy-red"></span>
+                    )}
+                  </span>
+                </Link>
+              </li>
+              <li>
+                <Link href="/Menu" className="m-2 Title1 inline-flex">
+                  <span className="relative">
+                    Menu
+                    {activeLink === '/Menu' && (
+                      <span className="absolute inset-x-0 bottom-0 h-1 bg-yummy-red"></span>
+                    )}
+                  </span>
+                </Link>
+              </li>
+              <li>
+                <Link href="/Contact" className="m-2 Title1 inline-flex">
+                  <span className="relative">
+                    Contact
+                    {activeLink === '/Contact' && (
+                      <span className="absolute inset-x-0 bottom-0 h-1 bg-yummy-red"></span>
+                    )}
+                  </span>
+                </Link>
+              </li>
             </ul>
           </nav>
         </div>
